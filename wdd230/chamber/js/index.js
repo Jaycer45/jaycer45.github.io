48fdc8df8ae499e3b20b8589fd5eb98e63417647
Use your GitHub username <Enter your email address>
2022-10-15 14:52:12 -0700

    s

--- wdd230/chamber/js/index.js
+++ wdd230/chamber/js/index.js
@@ -13,3 +13,16 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 function toggleMenu() {
     console.log(document.getElementById("navigation").classList.toggle("hide"));
 }
+
+const d = new Date();
+let day = d.getDay()
+
+if (day === 1) {
+    document.getElementById("meet").style.display = "";
+}
+else if (day === 2) {
+    document.getElementById("meet").style.display = "";
+}
+else {
+    document.getElementById("meet").style.display = "none";
+}
