84546f61a9178f675d559e1a2ba41f8c73098cad
Use your GitHub username <Enter your email address>
2022-10-15 13:37:59 -0700

    g

--- wdd230/chamber/js/index.js
+++ wdd230/chamber/js/index.js
@@ -9,3 +9,7 @@ const datefieldUK = document.querySelector("aside");
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+function toggleMenu() {
+    console.log(document.getElementById("navigation").classList.toggle("hide"));
+}
